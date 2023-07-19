71996572c4beb5b4535f0d3c8e2d5d72a335d94f
Viacheslav Saprykin <saprykinvp@gmail.com>
2023-07-19 17:18:14 +0300

    react_marvel_hooks__ver.5 - upd

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 import AppHeader from "../appHeader/AppHeader";
 import Spinner from '../spinner/Spinner';
